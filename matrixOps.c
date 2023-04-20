3b480bfc41cc4ccf5803a88ca2c536a2d71681dd
PedroZappa <pedrogzappa@gmail.com>
2023-04-20 20:45:59 +0100

    Operator Selection: DONE

--- matrixOps.c
+++ matrixOps.c
@@ -3,7 +3,11 @@
 int main() {
   const int ROWS = 2;
   const int COLS = 2;
+  char op;
   int a[ROWS][COLS], b[ROWS][COLS], sum[ROWS][COLS], diff[ROWS][COLS], i, j;
+
+  printf("Select Matrix Operation [ +, -, /, * ]:\n");
+  scanf("%c", &op);
 
   printf("Enter two 2x2 Matrices: \n");
   printf("          _ _ _ _ \n");
@@ -13,25 +17,37 @@ int main() {
   printf("Matrix 2: ");
   scanf("%d %d %d %d", &b[0][0], &b[0][1], &b[1][0], &b[1][1]);
 
-  // Adding two matrices
-  for (i = 0; i < ROWS; i++) { // Traverse Rows
-    for (j = 0; j < COLS; j++) { // Traverse Columns
-      sum[i][j] = a[i][j] + b[i][j];
-    }
+  switch (op) {
+    case '+': // Adding two matrices
+      for (i = 0; i < ROWS; i++) { // Traverse Rows
+        for (j = 0; j < COLS; j++) { // Traverse Columns
+          sum[i][j] = a[i][j] + b[i][j];
+        }
+      }
+      break;
+    case '-': // Subtracting two matrices
+      for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) { 
+          diff[i][j] = a[i][j] - b[i][j];
+        }
+      }
+      break;
+    // Operator doesn't match any case
+    default:
+      printf("Error! Invalid operator.\n");
+      break;
   }
 
-  // Subtracting two matrices
-  for (i = 0; i < ROWS; i++) {
-    for (j = 0; j < COLS; j++) { 
-      diff[i][j] = a[i][j] - b[i][j];
-    }
-  }
-
-  // Print the sum
-  printf("Matrix 1 + Matrix 2: \n");
+  // Print Operations
+  printf("Matrix 1 %c Matrix 2: \n", op);
   for (i = 0; i < ROWS; i++) {
     for (j = 0; j < COLS; j++) {
-      printf("%d ", sum[i][j]);
+      // Print results based on selected operation
+      if ( op == '+' ) {
+        printf("%d ", sum[i][j]);
+      } else if ( op == '-' ) {
+        printf("%d ", diff[i][j]);
+      }
       // Print a newline
       if (j == 1) {
         printf("\n");
